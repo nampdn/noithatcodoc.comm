3a863f1d26c0aac21f00c3b4d01c77fd12edeb34
Nam Pham <nampdn@protonmail.com>
2018-04-12 17:15:35 +0700

    refactor(config): change site title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Nội Thất Cơ Đốc',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -7,7 +7,7 @@ import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
+    <Helmet title="Nội Thất Cơ Đốc" />
     <Navbar />
     <div>{children()}</div>
   </div>
