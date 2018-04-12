6f74edee89e62f3a56512cb08e40262d318d1396
Nam Pham <nampdn@protonmail.com>
2018-04-12 17:47:39 +0700

    refactor(product): update product page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import logo from '../img/ntcd.png'
 
 const Navbar = () => (
   <nav className="navbar is-transparent">
@@ -15,24 +15,14 @@ const Navbar = () => (
         </Link>
       </div>
       <div className="navbar-start">
-        <Link className="navbar-item" to="/about">
-          About
-        </Link>
         <Link className="navbar-item" to="/products">
-          Products
+          Sản Phẩm
+        </Link>
+        <Link className="navbar-item" to="/about">
+          Về Chúng Tôi
         </Link>
       </div>
       <div className="navbar-end">
-        <a
-          className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="icon">
-            <img src={github} alt="Github" />
-          </span>
-        </a>
       </div>
     </div>
   </nav>
